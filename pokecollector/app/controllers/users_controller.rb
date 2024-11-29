43c100ae047aaa1d426d4b6ac197370a3219c276
Chase White <chase@Chases-MacBook-Pro.local>
2024-11-29 12:55:36 -0700

    added ability to see owned cards when viewing account page

--- pokecollector/app/controllers/users_controller.rb
+++ pokecollector/app/controllers/users_controller.rb
@@ -9,6 +9,8 @@ class UsersController < ApplicationController
 
   # GET /users/1 or /users/1.json
   def show
+    @user = User.find(params[:id]) # Find user by ID from URL
+    @cards = @user.cards           # Fetch cards owned by this user
   end
 
   # GET /users/new
